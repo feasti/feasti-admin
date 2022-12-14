20d85d4148ac58facaa7bba9292743b1a487a423
sumit245 <sumitranjan245@gmail.com>
2022-12-15 01:05:32 +0530

    Message

--- api/updater.js
+++ api/updater.js
@@ -4,16 +4,9 @@ const Orders = require('../models/orders.model')
 const NewRestaurant = require('../models/newrest.model')
 const currentOrder = require('../models/currentorders.model')
 
-// module.exports = function () {
-//     //  const orders=await Orders.find({status:'accepted'})
-//     cron.schedule('*/1 * * * *', () => {
-//         console.log('running on Sundays of January and September');
-//     });
-// }
 
 const job = cron.schedule('*/2 * * * *', async () => {
     const today = moment().format('DD-MMM-YYYY')
-    // const today=moment().forma
     await Orders.find({ $and: [{ start_date: today }, { status: 'accepted' }] }).updateMany({}, { $set: { status: 'started' } })
 });
 
