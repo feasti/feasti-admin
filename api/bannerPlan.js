136b023d6faf566b65cd4da42b64b8825bbb6a91
sumitranjan245@gmail.com <sumitranjan245@gmail.com>
2023-10-09 22:03:51 +0530

    message

--- api/bannerPlan.js
+++ api/bannerPlan.js
@@ -2,22 +2,12 @@ const express = require("express");
 const router = express.Router();
 const Banner = require("../models/bannerplan.model");
 
-/**
- * Retrieves all banner plans from the database and sends them as a JSON response.
- * 
- * @route GET /
- * @returns {Object} JSON response with status code, fetched plans, and success message
- */
 router.route("/").get(async (req, res) => {
   const banner = await Banner.find();
   res.json({ status: 200, data: banner, msg: "Plans Fetched" });
 });
 //get all plans
 
-/**
- * This code snippet defines routes for handling HTTP requests related to banner plans.
- * It includes routes for adding a new plan, fetching a specific plan, updating a plan, and deleting a plan.
- */
 
 router.route("/").post(async (req, res) => {
   const newBanner = new Banner(req.body);
